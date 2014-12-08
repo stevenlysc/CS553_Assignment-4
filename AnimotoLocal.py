9771cee853b2c26d20dbee397703d9575b99f76a
stevenlysc <stevenlysc@gmail.com>
2014-12-07 18:44:12 -0600

    local animoto

--- AnimotoLocal.py
+++ AnimotoLocal.py
@@ -57,41 +57,12 @@ class Animoto(object):
 			file_key = bucket.get_key(os.path.basename(source_path))
 			# url is valid for 5 minutes
 			url = file_key.generate_url(300)
-			msg = Message()
-			msg.set_body(url)
-			resultQueue.write(msg)
-			print '\tSending result ({}) to resultQueue.\n' .format(url)
+			print '\tURL: {}.\n' .format(url)
 		return
 
 	def generateVideo(self):
-		sqsConn = boto.sqs.connect_to_region('us-west-2')
-		dynamodbConn = boto.dynamodb.connect_to_region('us-west-2')
-		
-		taskQueue = sqsConn.get_queue('taskQueue')
-		myTable = dynamodbConn.get_table('MyTable')
-
-		while 1:
-			rs = taskQueue.get_messages()
-			if len(rs) == 0:
-				pass
-			else:
-				task = rs[0].get_body()
-				taskId = str(task).split(':')[0]
-				taskContent = str(task).split(':')[1]
-				if myTable.has_item(hash_key=taskId):
-					taskQueue.delete_message(rs[0])
-				else:
-					# Store into DynamoDB
-					item_data = {'taskContent': taskContent}
-					item = myTable.new_item(hash_key=taskId, attrs=item_data)
-					item.put()
-					# Execute task
-					urls = task.split(' ')
-					# Download image files
-					for index in range(len(urls)):
-						call('wget {} -O /Users/WayneHu/Desktop/pic/pic{}.jpg' .format(urls[index].strip(), str(index).zfill(3)), shell=True)
-					# create animotp
-					call('ffmpeg -i "/Users/WayneHu/Desktop/pic/pic%d.jpg" -c:v libx264 -preset ultrafast -qp 0 -filter:v "setpts=25.5*PTS" /Users/WayneHu/Desktop/pic/out{}.mkv' .format(str(index).zfill(3)))
+		call('sh /Users/WayneHu/Desktop/pic/list.sh', shell=True)
+		# create animotp
 		return
 
 	def startAnimoto(self):
