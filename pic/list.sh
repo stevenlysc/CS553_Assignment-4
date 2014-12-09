f21dd8aa53e90dfd76ebc6141f6a6d1ff3615d0d
Boyang <stevenlysc@gmail.com>
2014-12-09 00:43:51 -0600

    a

--- pic/list.sh
+++ pic/list.sh
@@ -4,4 +4,4 @@ cd /home/ubuntu/CS553_Assignment4/pic
 wget -i pic.txt >> /home/ubuntu/Log$1.txt
 x=1; for i in *jpg; do counter=$(printf %d $x); ln -s "$i" /home/ubuntu/CS553_Assignment4/pic/pic"$counter".jpg; x=$(($x+1)); done
 ffmpeg -i 'pic%d.jpg' -c:v libx264 -preset ultrafast -qp 0 -filter:v "setpts=25.5*PTS" out$1.mkv >> /home/ubuntu/Log$1.txt
-rm *.jpg
+rm *.jpg*
