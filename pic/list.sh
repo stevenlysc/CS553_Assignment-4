b26f86f7188a4e0c43afd7dc8538362a1a73a217
Boyang <stevenlysc@gmail.com>
2014-12-08 21:49:59 -0600

    a

--- pic/list.sh
+++ pic/list.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-cd /Users/WayneHu/Desktop/pic
+cd /home/ubuntu/CS553_Assignment4/pic
 wget -i pic.txt
 x=1; for i in *jpg; do counter=$(printf %d $x); ln -s "$i" /Users/WayneHu/Desktop/pic/pic"$counter".jpg; x=$(($x+1)); done
 ffmpeg -i 'pic%d.jpg' -c:v libx264 -preset ultrafast -qp 0 -filter:v "setpts=25.5*PTS" out.mkv
