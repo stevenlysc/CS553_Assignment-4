718319036f17bdd97239c4a640f4979cd169d93d
Boyang <stevenlysc@gmail.com>
2014-12-08 21:45:04 -0600

    a

--- RemoteWorker.py
+++ RemoteWorker.py
@@ -80,7 +80,7 @@ class RemoteWorker(object):
 	def uploadVideo(self):
 		print 'Uploading video to S3...'
 		s3Conn = S3Connection()
-		sqsConn = boto.s3.connect_to_region('us-west-2')
+		sqsConn = boto.sqs.connect_to_region('us-west-2')
 		resultQueue = sqsConn.get_queue('resultQueue')
 		# Try to create a new bucket if it does not exist
 		try:
